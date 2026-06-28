9e9d5217acbf30b23311eeb4e351d493849c8c02
Govinda <govinda777@gmail.com>
2026-06-28 18:21:23 -0300

    delete

--- src_old/modules/agreement/domain/repositories/agreement-repository.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface IAgreementRepository {
-  save(agreement: any): Promise<void>;
-  findById(id: string): Promise<any>;
-  findBySubscriberId(subscriberId: string): Promise<any[]>;
-}
